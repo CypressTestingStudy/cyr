5bafd996fb4d6919424fc2d977925d018d6d4999
christian <cristiantbon@gmail.com>
2021-02-08 10:45:53 -0500

    Cganges in order US21

--- cypress/integration/us21_egiftCar/us21_egiftCar.spec.js
+++ cypress/integration/us21_egiftCar/us21_egiftCar.spec.js
@@ -3,17 +3,22 @@
 describe("US21 eGiftCar", () => {
   it("Find eGiftcar", () => {
     cy.visit("https://www.amazon.com/ref=nav_logo");
-    cy.get("#nav-hamburger-menu").click().wait(2000);
+    cy.get("#nav-hamburger-menu")
+    .click()
+    .wait(2000);
 
-    cy.get("a.hmenu-item").eq(27).click().wait(2000);
+    cy.get("a.hmenu-item")
+    .eq(27).click()
+    .wait(2000);
 
-    cy.get("a.hmenu-item").contains("eGift cards").click().wait(2000);
+    cy.get("a.hmenu-item")
+    .contains("eGift cards")
+    .click().wait(2000);
 
-    cy.get('div[data-asin="B07PCMWTSG"]').click();
+    cy.get('div[data-asin="B07PCMWTSG"]')
+    .click();
 
-    cy.get('span[id="gc-asin-title"]').should(
-      "have.text",
-      "Amazon.com eGift Card"
-    );
+    cy.get('span[id="gc-asin-title"]')
+    .should("have.text","Amazon.com eGift Card" );
   });
 });

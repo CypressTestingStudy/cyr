90c27fe24b392a19d75edcae58f7034b750569f3
unknown <egerstner@gmail.com>
2021-02-09 09:40:25 -0500

    primer intento de Gersnter

---
+++ cypress/integration/addAvailableItem.spec.js
@@ -0,0 +1,19 @@
+describe("Add available Item to the cart", function () {
+  it("Select more than one product and add to the cart", function () {
+    cy.visit("https://www.amazon.com/");
+    cy.wait(1500);
+    cy.get('input[id="twotabsearchtextbox"]').type("xiaomi note 9 case");
+    cy.get('input[id="nav-search-submit-button"]').click();
+    cy.get(
+      'img[src="https://m.media-amazon.com/images/I/81hV6CPuX2L._AC_UL320_.jpg"]'
+    )
+      .first()
+      .click();
+    cy.wait(500);
+    cy.get('span[id="a-autoid-0"]').click();
+    let cantidad = 6;
+    cy.get('div[class="a-popover-inner"]').contains(cantidad).click();
+    cy.get('input[id="add-to-cart-button"]').click();
+    cy.get('span[id="nav-cart-count"]').should("contain", cantidad);
+  });
+});

060c280152119b38cb50412290f336963521c24b
Hilder Yesid Ospina Valencia <hilder.ospina@globant.com>
2021-02-09 10:10:19 -0500

    US14-buyNowWithoutLogeedIn
    changes for testing with USA locators and some updates for gitignore

--- cypress/integration/buyNowWithoutLogin/buyNowWithoutLogin.spec.js
+++ cypress/integration/buyNowWithoutLogin/buyNowWithoutLogin.spec.js
@@ -4,7 +4,7 @@ describe('Buy Now button without Log-in', () => {
     beforeEach( () => {
         cy.visit('https://www.amazon.com/')
     })
-    it('Buy Now', () => {
+    it('Buy Now without logged in', () => {
         //cy.get('#\\30 70443fb-0077-4bbf-9c58-b555a179ccca > div.a-section.a-spacing-none.feed-carousel > div > ul > li:nth-child(3) > span > a > img').click();
         //cy.get('#aeba0a24-9083-41a2-9d1d-fcabd620dca7 > .a-spacing-none > .a-section > .a-unordered-list').click();
         cy.get('#twotabsearchtextbox').type('PS4');
@@ -13,6 +13,6 @@ describe('Buy Now button without Log-in', () => {
         cy.get('#buy-now-button').click();   
         cy.wait(3000);
         cy.get('#siAddCoverage-announce').click(); 
-        cy.get('.a-padding-extra-large > .a-spacing-small').should('contain',"Sign-In");       
+        cy.get('.a-padding-extra-large > .a-spacing-small').should('contain',"Sign-In");  
     })
 })

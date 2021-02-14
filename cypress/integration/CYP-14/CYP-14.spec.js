38096056cae8e11d0f24bb62ba133302df511fee
rodrigo_alvarez <68355701+rodrigoaaam@users.noreply.github.com>
2021-02-13 20:54:54 -0500

    CYP-14 ok

---
+++ cypress/integration/CYP-14/CYP-14.spec.js
@@ -0,0 +1,8 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+import { CYP14 } from "../../Functions/CYP-14";
+
+describe("my first case with functional design", function () {
+  it("My first test with other selectors and new tab", CYP14);
+});

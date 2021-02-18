0f8a8b73d82e3f43926803a18ccc8e265e1cb48a
unknown <egerstner@gmail.com>
2021-02-18 14:48:01 -0500

    CYP 12 CYP 13 CYP 19 V2

--- cypress/integration/SelectAvailableItemCart.js
+++ cypress/integration/SelectAvailableItemCart.js
@@ -8,7 +8,7 @@ import {
   checkCart,
   selectProduct2,
   buyNowItem,
-} from "../AmazonFunctions/SelectProduct";
+} from "../CYP 12,13,19/SelectProduct";
 describe("Add available Item to the cart", function () {
   it("Select more than one product and add to the cart", function () {
     searchPage();

59d27ad1ed9144d0bf612baa295eb98a52b32651
rodrigo_alvarez <68355701+rodrigoaaam@users.noreply.github.com>
2021-02-19 12:59:23 -0500

    forgot password

---
+++ cypress/Functions/CYP-24.js
@@ -0,0 +1,14 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+export function CYP24() {
+  cy.visit(
+    "https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&"
+  );
+
+  cy.wait(3000);
+
+  cy.get(".a-expander-prompt").click();
+
+  cy.get("#auth-fpp-link-bottom").click();
+}

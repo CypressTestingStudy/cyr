912cb5c0fb811fc8b86dd5bebde497602b7fcbf1
Daniel Fuentes <eduwin.fuentes@outlook.com>
2021-03-15 10:44:52 -0500

    CYP-56 Adding more test cases

--- cypress/integration/youtube/cyp-56-gammingTest.spec.js
+++ cypress/integration/youtube/cyp-56-gammingTest.spec.js
@@ -4,14 +4,50 @@ describe("TS Gaming Video Player - Youtube", function () {
   
   before(function () {
     cy.selectSectionYT('Videojuegos', 'Fortnite');
-    cy.contains('OFICIAL').click().wait(3000);
+    cy.contains('LAST').click().wait(3000);
   });
 
    it("[TC01] Pause button check", function () {
-    cy.get('.ytp-play-button').click();
+    cy.get('.ytp-play-button').should("be.visible").click();
    });
 
    it("[TC02] Next button check", function () {
-    cy.get('.ytp-next-button').click();
+    cy.get('.ytp-next-button').should("be.visible").click();
    });
+
+   it("[TC03] Mute button check", function () {
+    cy.get('.ytp-mute-button').should("be.visible").click();
+   });
+
+   it("[TC04] Settings button check", function () {
+    cy.get('.ytp-settings-button').should("be.visible").click();
+   });
+
+   it("[TC05] Automatic Repro button check", function () {
+    cy.get('.ytp-autonav-toggle-button-container').should("be.visible").click();
+   });
+
+   it("[TC06] Big Size Repro button check", function () {
+    cy.get('.ytp-size-button').should("be.visible").click();
+   });
+
+   it("[TC07] FullScreen Repro button check", function () {
+    cy.get('.ytp-fullscreen-button').should("be.visible").click();
+   });
+   
+   it("[TC08] Mini Repro button check", function () {
+    cy.get('.ytp-miniplayer-button').should("be.visible").click();
+   });
+
+
+   it("[TC09] Mini Repro button check", function () {
+    cy.get('.ytp-ad-skip-button').should("be.visible").click();
+   });
+
+   it("[TC10] Mini Repro go back button check", function () {
+    cy.get('.ytp-miniplayer-scrim').should("be.visible").click();
+   });
+   
+   
+
 });

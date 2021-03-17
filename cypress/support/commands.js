e11fb39a7909de06b607da0100ac2e7e271e61ab
[Hlperez] <hlperez@gmail.com>
2021-03-17 09:05:47 -0500

    youtube test final version

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,16 +26,39 @@
 
 import "@testing-library/cypress/add-commands";
 
-Cypress.Commands.add('amazonWebSite', function () {
-        cy.visit('https://www.amazon.com/');
-        cy.wait(3000);
+Cypress.Commands.add("amazonWebSite", function () {
+  cy.visit("https://www.amazon.com/");
+  cy.wait(3000);
 });
 
-Cypress.Commands.add('loginAmazon', function (user) {
-        cy.visit('https://www.amazon.com/');
-        cy.contains('Sign in').click();
-        cy.get('#ap_email').clear().type(user.email);
-        cy.get('.a-button-inner > #continue').click();
-        cy.get('#ap_password').clear().type(user.pwd);
-        cy.get('#signInSubmit').click();
+Cypress.Commands.add("loginAmazon", function (user) {
+  cy.visit("https://www.amazon.com/");
+  cy.contains("Sign in").click();
+  cy.get("#ap_email").clear().type(user.email);
+  cy.get(".a-button-inner > #continue").click();
+  cy.get("#ap_password").clear().type(user.pwd);
+  cy.get("#signInSubmit").click();
+});
+
+Cypress.Commands.add("visitYoutube", function () {
+  cy.visit("https://www.youtube.com");
+});
+
+Cypress.Commands.add("reproduceVideo", function (category) {
+  const myCategory = 'a[title="' + category + '"]';
+  cy.get("#guide-icon").click();
+  cy.get(myCategory, { timeout: 3000 }).click();
+  cy.wait(2000);
+  cy.get('[title="Sports"]').contains("Sports");
+  cy.get("#hero-title").click({ force: true });
+  cy.get("div#container.style-scope.ytd-playlist-panel-renderer")
+    .eq(0)
+    .then(function (video) {
+      return video[0].baseURI;
+    })
+    .as("videoURL");
+  cy.get("@videoURL").then((url) => {
+    cy.visit(url);
+    cy.wait(30000);
+  });
 });

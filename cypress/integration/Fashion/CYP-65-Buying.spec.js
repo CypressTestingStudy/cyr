2ebd86415cbce43d6503c2760e15e4c1a18b7013
Daniel Fuentes <eduwin.fuentes@outlook.com>
2021-04-14 13:56:24 -0500

    CYP-65 Initial Commit

---
+++ cypress/integration/Fashion/CYP-65-Buying.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe("Checking cart and sign out", () => {
+   
+    beforeEach(() => {
+        cy.loginFashionPage({ email: "rodrigo032792@gmail.com", pwd: "Club2021*" });
+    });
+    it("TC01 - Buying a articule", () => {
+      
+      
+    });
+  });

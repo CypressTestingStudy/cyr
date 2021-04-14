e2807ba09ad7b7e63e17789d284d368e5d5aff76
rodrigo_alvarez <68355701+rodrigoaaam@users.noreply.github.com>
2021-04-14 09:29:44 -0500

    login precondition for the rest of the cases

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,7 +2,15 @@ import "@testing-library/cypress/add-commands";
 
 Cypress.Commands.add("amazonWebSite", function () {
   cy.visit("https://www.amazon.com/");
+});
 
+Cypress.Commands.add("loginFashionPage", function (user) {
+  cy.visit("http://automationpractice.com/index.php");
+  cy.get(".login").click();
+  cy.get("#email").type(user.email);
+  cy.get("#passwd").type(user.pwd);
+  cy.get("#SubmitLogin > span").click();
+  cy.get(".logout").contains("Sign out");
 });
 
 Cypress.Commands.add("loginAmazon", function (user) {
@@ -14,17 +22,16 @@ Cypress.Commands.add("loginAmazon", function (user) {
   cy.get("#signInSubmit").click();
 });
 
-Cypress.Commands.add("selectSectionYT", function (label, sectionTab ) {
+Cypress.Commands.add("selectSectionYT", function (label, sectionTab) {
   cy.visit("https://www.youtube.com/");
   cy.get("#guide-button").click();
   cy.contains(label).click();
   cy.get("ytd-game-details-renderer")
-  .find("a")
-  .each(function ($el, index, $list) {
-    const textSectionTab = $el.find("#title").text();
-    if (textSectionTab.includes(sectionTab)) {
-      $el.find("#img").click();
-    }
-  });
+    .find("a")
+    .each(function ($el, index, $list) {
+      const textSectionTab = $el.find("#title").text();
+      if (textSectionTab.includes(sectionTab)) {
+        $el.find("#img").click();
+      }
+    });
 });
-

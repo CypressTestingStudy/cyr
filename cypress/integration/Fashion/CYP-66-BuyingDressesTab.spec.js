d1c47b48da353f2abee93f62d7373b156dc935d8
EllaGerstner <egerstner@gmail.com>
2021-04-21 10:13:45 -0500

    Second attempt to push this test case

--- cypress/integration/Fashion/CYP-66-BuyingDressesTab.spec.js
+++ cypress/integration/Fashion/CYP-66-BuyingDressesTab.spec.js
@@ -7,12 +7,6 @@ describe("Buying dresses", () => {
     const getIframeDocument = () => {
         return cy
             .get("iframe[class='fancybox-iframe']")
-            // Cypress yields jQuery element, which has the real
-            // DOM element under property "0".
-            // From the real DOM iframe element we can get
-            // the "document" element, it is stored in "contentDocument" property
-            // Cypress "its" command can access deep properties using dot notation
-            // https://on.cypress.io/its
             .its('0.contentDocument').should('exist')
     }
 
@@ -31,63 +25,73 @@ describe("Buying dresses", () => {
         dresses.getTabDresses().click();
         dresses.getCasualDresses().click();
         dresses.getQuickView().click();
-        cy.wait(6500);
+        cy.wait(12000);
+        getIframeBody().find("#group_1").select("L");
         getIframeBody().find('#wishlist_button').click();
+        cy.wait(2000);
         getIframeBody().find("p[class='fancybox-error']").contains('Added to your wishlist');
+        cy.wait(2000);
         getIframeBody().find("a[title='Close']").click();
         cy.wait(1000);
-        cy.get(".fancybox-item").click();
+        dresses.getCloseIframe().click();
     });
     it("Checkout Evening Dress", () => {
         const dresses = new Dresses();
-        cy.wait(1000);
+        cy.wait(3000);
         dresses.getTabDresses().click();
         dresses.getEveningDresses().click();
         dresses.getQuickView().click();
-        cy.wait(6500);
+        cy.wait(12000);
+        getIframeBody().find("#group_1").select("M");
         getIframeBody().find("i[class='icon-plus']").click();
         getIframeBody().find("a[class='color_pick']").click();
         getIframeBody().find("button[type='submit']").click();
-        cy.get("#layer_cart").should("contain.text", "Product successfully added to your shopping cart");
+        dresses.getConfirmationAddCart().should("contain.text", "Product successfully added to your shopping cart");
+        cy.wait(3000);
         cy.get("a[title='Proceed to checkout']").click();
         cy.get("a[href='http://automationpractice.com/index.php?controller=order&step=1']").click();
-        cy.get("button[name='processAddress']").click();
-        cy.get("input[type='checkbox']").click();
-        cy.get("button[name='processCarrier']").click();
-        cy.get("a[class='cheque']").click();
-        cy.get("button[class*='btn-default button-medium']").click();
-        cy.get("p[class*='alert-succes'").should('contain.text', "Your order on My Store is complete.");
+        dresses.getCheckOutAddress().click();
+        dresses.getAcceptTerms().click();
+        dresses.getConfirmCheckOut().click();
+        dresses.getChequePayment().click();
+        dresses.getFinalConfirm().click();
+        dresses.getConfirmationBuy().should('contain.text', "Your order on My Store is complete.");
     });
     it("Buying more than one Summer Dress", () => {
         const dresses = new Dresses();
-        cy.wait(1000);
+        cy.wait(3000);
         dresses.getTabDresses().click();
         dresses.getSummerDresses().click();
         dresses.getViewList().click();
         cy.contains("More").first().click();
+        dresses.getSizeChange().select("M");
         cy.get("#color_14").click();
-        cy.get("#add_to_cart").click();
-        cy.get("#layer_cart").should("contain.text", "Product successfully added to your shopping cart");
+        dresses.getAddToCart().click();
+        dresses.getConfirmationAddCart().should("contain.text", "Product successfully added to your shopping cart");
+        cy.wait(3000);
         cy.get("span[title='Continue shopping']").click();
-        cy.get('.breadcrumb > [href="http://automationpractice.com/index.php?id_category=11&controller=category"]').click();
-        cy.get('.last-in-line > .product-container > :nth-child(1) > .right-block > .right-block-content > .button-container > .lnk_view > span').click();
+        dresses.getSummerSection().click();
+        dresses.getInformationLastDress().click();
+        dresses.getSizeChange().select("L");
         cy.get("i[class='icon-plus']").click();
         cy.get("i[class='icon-plus']").click();
         cy.get("#color_15").click();
-        cy.get("#add_to_cart").click();
-        cy.get("#layer_cart").should("contain.text", "Product successfully added to your shopping cart");
+        dresses.getAddToCart().click();
+        dresses.getConfirmationAddCart().should("contain.text", "Product successfully added to your shopping cart");
+        cy.wait(3000);
         cy.get("span[title='Continue shopping']").click();
         cy.get("i[class='icon-minus']").click();
         cy.get("#color_16").click();
-        cy.get("#add_to_cart").click();
-        cy.get("#layer_cart").should("contain.text", "Product successfully added to your shopping cart");
+        dresses.getAddToCart().click();
+        dresses.getConfirmationAddCart().should("contain.text", "Product successfully added to your shopping cart");
+        cy.wait(500);
         cy.get("a[title='Proceed to checkout']").click();
         cy.get("a[href='http://automationpractice.com/index.php?controller=order&step=1']").click();
-        cy.get("button[name='processAddress']").click();
-        cy.get("input[type='checkbox']").click();
-        cy.get("button[name='processCarrier']").click();
-        cy.get("a[class='cheque']").click();
-        cy.get("button[class*='btn-default button-medium']").click();
-        cy.get("p[class*='alert-succes'").should('contain.text', "Your order on My Store is complete.");
+        dresses.getCheckOutAddress().click();
+        dresses.getAcceptTerms().click();
+        dresses.getConfirmCheckOut().click();
+        dresses.getChequePayment().click();
+        dresses.getFinalConfirm().click();
+        dresses.getConfirmationBuy().should('contain.text', "Your order on My Store is complete.");
     });
 });

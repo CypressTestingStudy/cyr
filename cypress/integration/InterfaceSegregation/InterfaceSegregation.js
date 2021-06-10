b4273a3a3c407989d82ec943996f2b98a4a29ed4
[Hlperez] <62311636+hlperez07@users.noreply.github.com>
2021-06-10 11:35:09 -0500

    interface segregation principle

---
+++ cypress/integration/InterfaceSegregation/InterfaceSegregation.js
@@ -0,0 +1,36 @@
+interface IAve {  
+    void comer();
+}
+interface IAveVoladora {  
+    void volar();
+}
+
+interface IAveNadadora {  
+    void nadar();
+}
+
+class Loro implements IAve, IAveVoladora{
+
+    @Override
+    public void volar() {
+        //...
+    }
+
+    @Override
+    public void comer() {
+        //...
+    }
+}
+
+class Pinguino implements IAve, IAveNadadora{
+
+    @Override
+    public void nadar() {
+        //...
+    }
+
+    @Override
+    public void comer() {
+        //...
+    }
+}
---
+++ cypress/integration/InterfaceSegregation/Non-InterfaceSegregation.js
@@ -0,0 +1,41 @@
+interface IAve {  
+    void volar();
+    void comer();
+    void nadar();
+}
+
+class Loro implements IAve{
+
+    @Override
+    public void volar() {
+        //...
+    }
+
+    @Override
+    public void comer() {
+        //...
+    }
+
+    @Override
+    public void nadar() {
+        //...
+    }
+}
+
+class Pinguino implements IAve{
+
+    @Override
+    public void volar() {
+        //...
+    }
+
+    @Override
+    public void comer() {
+        //...
+    }
+
+    @Override
+    public void nadar() {
+        //...
+    }
+}
